483a8c5bf5181dbf038ed3ea09339842b5377ebc
Sergio Garzon <sggarzonutn@gmail.com>
2026-08-15 12:01:26 -0300

    feat: new changes

--- Exercise1/exercise1.py
+++ Exercise1/exercise1.py
@@ -2,15 +2,16 @@
 from surtidor_class import Surtidor
 
 # Estación de servicio
-# Una estación de servicio que dispone de 10 surtidores y necesita gestionar
-# información relacionada con la venta de combustibles en la jornada.
+#
+# Una estación de servicio que dispone de 10 surtidores y necesita gestionar información relacionada 
+# con la venta de combustibles en la jornada. 
+# 
 # De cada surtidor se conoce:
 # • Número de Surtidor (validar que sea un número entre 1 y 30)
-# • Cantidad: representa la cantidad de litros de combustible vendido por
-# el surtidor (validar que sea un número positivo)
-# • Tipo: representa el tipo de combustible del surtidor. Los valores que pue-
-# de asumir son 1 representa “Nafta Super”, 2 representa “Nafta Especial”
-# y 3 representa “Gasoil” (validar que se ingresen valores válidos).
+# • Cantidad: representa la cantidad de litros de combustible vendido por el surtidor (validar que sea un número positivo)
+# • Tipo: representa el tipo de combustible del surtidor. Los valores que puede asumir son 
+# 1 representa “Nafta Super”, 2 representa “Nafta Especial” y 3 representa “Gasoil” (validar que se ingresen valores válidos).
+#
 # Se pide calcular e imprimir:
 # • El total de litros vendidos en la jornada, por tipo de combustible.
 # • El número de surtidor que menos combustible vendió.
@@ -36,7 +37,7 @@ def carga_surtidores():
     nro_surtidor = tipo = 0
     cantidad = -1
     
-    for value_into in range(10):
+    for value_into in range(3):
         
         print("\nDatos del surtidor " + str(value_into + 1) + ": ")
         
@@ -75,10 +76,24 @@ def litros_vendidos_combustibles(datos_surtidores):
             case 2: # Nafta Especial
                 litro_total_nafta_especial += dato.get_cantidad()                
             case 3: # Gasoil
-                litro_total_gasoil += dato.get_cantidad() 
-                            
+                litro_total_gasoil += dato.get_cantidad()                             
         
     return [litro_total_nafta, litro_total_nafta_especial, litro_total_gasoil]
+
+
+# Corregir
+def surtido_menos_combustible(datos_surtidores):
+    litro_vendido = int(datos_surtidores[0].get_cantidad())
+    dato_surtidor = int(datos_surtidores[0].get_nro_superior())
+    
+    for dato in datos_surtidores:
+        if litro_vendido < int(dato.get_cantidad()):
+            dato_surtidor = int(dato.get_nro_superior())
+            litro_vendido = int(dato.get_cantidad())    
+        else:
+            litro_vendido = int(dato.get_cantidad())                 
+            
+    return dato_surtidor   
     
 
 def promedio_general_cantidad(datos_surtidores):
@@ -95,6 +110,8 @@ datos_surtidores = carga_surtidores()
 print("El promedio total es: " + str(promedio_general_cantidad(datos_surtidores)))
 
 print(litros_vendidos_combustibles(datos_surtidores))
+
+print("Número de surtidor con menos venta: " + str(surtido_menos_combustible(datos_surtidores)))
 
 
 
